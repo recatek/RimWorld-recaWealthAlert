fcc5673342db4e6a84e85c13094074db0c2f200a
recatek <recatek@hotmail.com>
2025-08-10 12:09:41 -0400

    Initial Commit
    Tested gravship and caravan/camp cases

---
+++ README.md
@@ -0,0 +1,3 @@
+# RimWorld-recaWealthAlert
+
+Adds an alert (right-side persistent indicator) on the RimWorld UI of your current wealth for the current colony.
---
+++ Source/WealthAlert.cs
@@ -0,0 +1,37 @@
+﻿using RimWorld;
+using Verse;
+
+namespace recatek.WealthAlert
+{
+    [StaticConstructorOnStartup]
+    public class WealthAlertMod : Mod
+    {
+        public WealthAlertMod(ModContentPack content) : base(content)
+        {
+            // Nothing to do
+        }
+    }
+
+    public class Alert_WealthAlert : Alert
+    {
+        private static int GetWealth()
+        {
+            int wealth = 0;
+            if (Find.CurrentMap != null && Find.CurrentMap.wealthWatcher != null)
+                wealth += (int)Find.CurrentMap.wealthWatcher.WealthTotal;
+            if (Find.CurrentGravship != null && (Find.CurrentGravship.Tile == Find.CurrentMap?.Tile))
+                wealth += (int)Find.CurrentGravship.PlayerWealthForStoryteller;
+            return wealth;
+        }
+
+        public override string GetLabel()
+        {
+            return "recatek.WealthAlert.Label".Translate(GetWealth());
+        }
+
+        public override AlertReport GetReport()
+        {
+            return GetWealth() > 0 ? AlertReport.Active : AlertReport.Inactive;
+        }
+    }
+}
